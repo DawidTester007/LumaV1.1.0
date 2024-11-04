e1bd845e886f99f19a491fcc822491831503b8ac
Dawid <dawidkilian007@gmail.com>
2024-11-04 21:31:11 +0100

    Aktualizcja plków xml

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -3,7 +3,6 @@ package baseTest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Properties;
@@ -11,16 +10,12 @@ import java.util.Date;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -40,7 +35,7 @@ public class BaseTest {
 	public WebDriverWait DriverWait;
 	
 	
-	@BeforeMethod(groups ={"functional","regression","sanity","master"})
+	@BeforeMethod(groups ={"functional","regression","sanity","master","dataDriver"})
 	@Parameters({"system","browser"})
 	public void setUp(String system , String browser) throws IOException {
 		
@@ -91,7 +86,7 @@ public class BaseTest {
 		
 	}
 	
-	@AfterMethod(groups ={"functional","regression","sanity","master"})
+	@AfterMethod(groups ={"functional","regression","sanity","master","dataDriver"})
 	public void close() {
 		
 	  //zamknięcie strony
